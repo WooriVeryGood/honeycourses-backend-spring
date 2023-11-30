853a5556ad6761868b8a3cdc1592e3b68e13e807
Percy Jang <joonwoo_jang@pku.edu.cn>
2023-11-30 16:52:21 +0800

    project init

---
+++ src/main/java/org/wooriverygood/HoneycoursesBackend.java
@@ -0,0 +1,13 @@
+package org.wooriverygood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HoneycoursesBackend {
+    public static void main(String[]args) {
+        SpringApplication.run(HoneycoursesBackend.class, args);
+    }
+}
