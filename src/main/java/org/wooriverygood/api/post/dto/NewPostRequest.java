2bbf5aaa0adbc7ecf201db5b8380a6e4c68c154b
Park Geonho <pagh2322@naver.com>
2024-01-17 23:18:36 +0800

    [HC-69] 게시글 생성 엔드 포인트 RequestBody 수정
    기존 RequestBody에서는 post_author로 유저의 정보를 가져왔지만, 이제 AuthInfo로 가져온 유저의 메일로 게시글의 작성자를 설정합니다.
    
    PostService 테스트 코드도 수정했습니다.

--- src/main/java/org/wooriverygood/api/post/dto/NewPostRequest.java
+++ src/main/java/org/wooriverygood/api/post/dto/NewPostRequest.java
@@ -9,13 +9,11 @@ public class NewPostRequest {
     private final String post_title;
     private final String post_category;
     private final String post_content;
-    private final String email;
 
     @Builder
-    public NewPostRequest(String post_title, String post_category, String post_content, String email) {
+    public NewPostRequest(String post_title, String post_category, String post_content) {
         this.post_title = post_title;
         this.post_category = post_category;
         this.post_content = post_content;
-        this.email = email;
     }
 }
