f00160e0e27e4c79ef8b051509f87a1d914206c9
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-01-18 23:02:33 +0800

    [HC-103] 서비스들 Transactional 애노테이션 추가

--- src/main/java/org/wooriverygood/api/course/service/CourseService.java
+++ src/main/java/org/wooriverygood/api/course/service/CourseService.java
@@ -2,6 +2,7 @@ package org.wooriverygood.api.course.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.wooriverygood.api.course.domain.Courses;
 import org.wooriverygood.api.course.dto.CourseResponse;
 import org.wooriverygood.api.course.repository.CourseRepository;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class CourseService {
     private final CourseRepository courseRepository;
 
--- src/main/java/org/wooriverygood/api/review/service/ReviewService.java
+++ src/main/java/org/wooriverygood/api/review/service/ReviewService.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReviewService {
     private final ReviewRepository reviewRepository;
 
