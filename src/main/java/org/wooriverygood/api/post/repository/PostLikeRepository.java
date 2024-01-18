b8bcf5a90c0546ac02fdced8f3ca8256f93dbc2f
Park Geonho <pagh2322@naver.com>
2024-01-19 00:05:41 +0800

    [HC-74] 게시글 좋아요 기능 구현
    #### 서비스 테스트 코드
    
    현재 서비스 테스트 코드는 `@SpringBootTest`으로 인해 스프링 컨테이너를 띄우고 실행한다. 단위 테스트만 작성하기 때문에 비효율적이므로, `@ExtendWith(MockitoExtension.class)`으로 테스트 관련 객체들만 mock하여 테스트 속도를 더욱 빠르게 할 수 있음.

---
+++ src/main/java/org/wooriverygood/api/post/repository/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package org.wooriverygood.api.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.wooriverygood.api.post.domain.Post;
+import org.wooriverygood.api.post.domain.PostLike;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByPostAndUsername(Post post, String username);
+}
--- src/main/java/org/wooriverygood/api/post/repository/PostRepository.java
+++ src/main/java/org/wooriverygood/api/post/repository/PostRepository.java
@@ -1,6 +1,9 @@
 package org.wooriverygood.api.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import org.wooriverygood.api.post.domain.Post;
 
 import java.util.List;
@@ -8,4 +11,14 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByAuthor(String author);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE posts SET like_count = like_count + 1 WHERE post_id = :postId", nativeQuery = true)
+    void increaseLikeCount(Long postId);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE posts SET like_count = like_count - 1 WHERE post_id = :postId", nativeQuery = true)
+    void decreaseLikeCount(Long postId);
 }
